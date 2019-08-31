0a9d428d2e510d62b33e807697f30a358aeb302f
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-31 18:15:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
     item = coupon[item]
       if cart[item]
         if cart[item][:count] >= coupon[:num] && !cart.has.key?("#{item} W/COUPON")
-          cart
+          cart["#{item}"]
     
     
 end
