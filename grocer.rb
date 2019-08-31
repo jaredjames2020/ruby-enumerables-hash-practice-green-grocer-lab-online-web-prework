accaf54f8fcecd32e7122bc9d0b49b482f7cc977
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-31 18:42:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,6 +21,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
       if cart.has_key?(item)
+        binding.pry
         if cart[item][:count] >= coupon[:num] && !cart.has.key?("#{item} W/COUPON")
           cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count:[coupon][:num]}
         elsif cart[item][:count] >= coupon[:num] && cart.has_key?["#{item} W/COUPON"]
@@ -29,7 +30,6 @@ def apply_coupons(cart, coupons)
           cart[item][:count] -= coupon[:num]
       end
     end 
-    binding.pry
   cart  
 end
 
