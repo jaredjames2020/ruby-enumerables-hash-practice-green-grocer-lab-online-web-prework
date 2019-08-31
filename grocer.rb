e8cecbf9c2e68839ae4ff08fc722472d66927ab3
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-31 20:07:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,20 @@ def apply_coupons(cart, coupons)
       end
     end 
   cart  
-end
+#     coupons.each do |coupon|
+#     item = coupon[:item]
+#     if cart.has_key?(item)
+#       if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+#         cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+#         cart[item][:count] -= coupon[:num]
+#       elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
+#       cart["#{item} W/COUPON"][:count] += coupon[:num]
+#       cart[item][:count] -= coupon[:num]
+#       end
+#     end
+#   end
+#   cart
+# end
 
 def apply_clearance(cart)
   # code here
