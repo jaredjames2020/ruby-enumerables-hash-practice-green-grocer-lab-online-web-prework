122e0324c4dacfa323412431124c2d67fce63083
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-31 18:10:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,22 +20,9 @@ def apply_coupons(cart, coupons)
   # code here
   coupons.each do |product|
     item = coupon_hash[:item]
-    new_coupon_hash = {
-      :price => coupon_hash[:cost],
-      :clearance => "true",
-      :count => coupon_hash[:num]
-    }
+      if 
     
-     if cart.key?(fruit_name)
-      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
-      if cart[fruit_name][:count]>= new_coupon_hash[:count]
-        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
-        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
-      end
-      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
-    end
-    end
-  return cart
+    
 end
 
 
