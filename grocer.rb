0f3918aa7611249a43ac5650b51744497e3e6c34
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-31 20:10:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,20 +55,3 @@ def checkout(cart, coupons)
     total
   end
 end
-
-
-#def consolidate_cart(cart)
-  # code here
-#end
-
-# def apply_coupons(cart, coupons)
-#   # code here
-# end
-
-# def apply_clearance(cart)
-#   # code here
-# end
-
-# def checkout(cart, coupons)
-#   # code here
-# end
